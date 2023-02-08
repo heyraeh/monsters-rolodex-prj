1666e517e22b8af70fb17bc180f23b2e4c293444
heyraeh <heyraeh@gmail.com>
2023-02-08 13:17:07 -0800

    first commit of full files

---
+++ src/App.js
@@ -0,0 +1,61 @@
+import { Component } from 'react';
+
+import CardList from './components/card-list/card-list.component';
+import SearchBox from './components/search-box/search-box.component';
+import './App.css';
+
+class App extends Component {
+  constructor() {
+    super();
+    
+    this.state = {
+      monsters: [],
+      searchField: ''
+    };
+    // console.log('Constructor');
+  }
+
+  componentDidMount() {
+    // console.log('componentDidMount');
+
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((users) => this.setState(() => {
+        return {monsters: users}
+      },
+      () => {console.log(this.state);}
+      ));
+  }
+
+  onSearchChange = (event) => {
+    const searchField = event.target.value.toLocaleLowerCase();
+    this.setState(() => {
+      return { searchField };
+    })
+
+  }
+
+  render() {
+    // console.log('render from AppJS');
+
+    const { monsters, searchField } = this.state;
+    const { onSearchChange } = this;
+
+    const filteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField);
+    });
+
+    return (
+      <div className="App">
+        <SearchBox 
+        className='monsters-search-box'
+        onChangeHandler={onSearchChange} 
+        placeholder='search monsters' 
+        />
+        <CardList monsters={filteredMonsters}/>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/card-list/card-list.component.jsx
@@ -0,0 +1,31 @@
+import { Component } from "react";
+
+import './card-list.styles.css';
+
+class CardList extends Component {
+
+    render() {
+        const { monsters } = this.props;
+
+        return (
+                <div> 
+                   {monsters.map(monster => {
+                    const { name, email, id } = monster;
+                    
+                    return (
+                    <div className='card-container' key={id} >
+                    <img 
+                        alt={`monster ${name}`} 
+                        src={`https://robohash.org/${id}?set=set4&size=180x180`}
+                    /> 
+                    <h2>{name}</h2>
+                    <p>{email}</p>
+                    
+                    </div>
+                   )})}
+                </div>
+            )
+        }
+    }
+
+export default CardList;
